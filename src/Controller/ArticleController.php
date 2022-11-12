14ba6688714206a8e0ca016fa2636e7249cab143
Anthony Ladon <ladonanthony@gmail.com>
2022-11-12 12:50:59 +0100

    vue tableau pair/impair

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -18,4 +18,38 @@ class ArticleController extends AbstractController
             'controller_name' => 'ArticleController',
         ]);
     }
+
+    /**
+     * @route("/article/list", name="arrays")
+     */
+    public function array(): Response
+    {
+        $nb = [];
+        for ($i = 0; $i < 10; $i++) {
+            // rand() crée un chiffre random, array_push l'insere dans le tab
+            $random_int = rand(1, 100);
+            array_push($nb, $random_int);
+        }
+
+        return $this->render(
+            'article/list.html.twig',
+            [
+                'titre_nb' => 'Voici la liste des items:',
+                'tab_nombres' => $nb
+            ]
+        );
+    }
+
+    /**
+     * @route("/article/string")
+     */
+    public function array_string(): Response
+    {
+        return $this->render(
+            'article/string.html.twig',
+            [
+                'titre_string' => 'Voici le premier item du tableau de string:'
+            ]
+        );
+    }
 }
